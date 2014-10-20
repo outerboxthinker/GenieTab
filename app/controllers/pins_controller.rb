ab499cfc6009a512d7dda283da87cafe064d50b8
OurterBox Thinker <outerboxthinker@gmail.com>
2014-10-20 15:53:14 -0700

    Configured Pin/User association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,7 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
-
+  before_action :correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
  
   def index
     @pins = Pin.all
@@ -10,14 +11,19 @@ class PinsController < ApplicationController
   end
 
   def new
-    @pin = Pin.new
+    @pin = current_user.pins.build
   end
 
   def edit
   end
 
   def create
-    @pin = Pin.new(pin_params)
+    @pin = current_user.pins.build(pin_params)
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render action: 'new'
+    end
 
     respond_to do |format|
       if @pin.save
@@ -54,6 +60,11 @@ class PinsController < ApplicationController
     
     def set_pin
       @pin = Pin.find(params[:id])
+    end
+
+    def correct_user
+      @Pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     def pin_params
