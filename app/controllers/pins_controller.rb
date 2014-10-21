3c1bdd7ce238b4241ddd540aa20eb8acbd314586
OurterBox Thinker <outerboxthinker@gmail.com>
2014-10-20 22:10:56 -0700

    Configured pin/user association

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -24,50 +24,34 @@ class PinsController < ApplicationController
     else
       render action: 'new'
     end
-
-    respond_to do |format|
-      if @pin.save
-        format.html { redirect_to @pin, notice: 'Pin was successfully created.' }
-        format.json { render :show, status: :created, location: @pin }
-      else
-        format.html { render :new }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def update
-    respond_to do |format|
       if @pin.update(pin_params)
-        format.html { redirect_to @pin, notice: 'Pin was successfully updated.' }
-        format.json { render :show, status: :ok, location: @pin }
+        redirect_to @pin, notice: 'Pin was successful created.'
       else
-        format.html { render :edit }
-        format.json { render json: @pin.errors, status: :unprocessable_entity }
+        render action: 'edit'
       end
     end
-  end
 
   def destroy
     @pin.destroy
-    respond_to do |format|
-      format.html { redirect_to pins_url, notice: 'Pin was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to pins_url
   end
-
+  
   private
-    
+    # Use callbacks to share common setup or constraints between actions.
     def set_pin
       @pin = Pin.find(params[:id])
     end
-
+    
     def correct_user
       @Pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
+    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
-      params.require(:pin).permit(:description)
+      params.require(:pin).permit(:description, :image)
     end
 end
